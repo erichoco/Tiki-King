07607ec8fdeedfbd4f4f12bedc517d1764398d0f
Eric Chiu <wf870125@gmail.com>
2013-11-22 22:45:12 +0800

    Pull from github 11/12

---
+++ js/testAI.js
@@ -0,0 +1,17 @@
+/* Implement AI here */
+
+(function() {
+
+$(document).ready(function() {
+
+        console.log("Test it");
+        test();
+
+});
+
+function test() {
+    console.log("test function");
+}
+
+
+})();
