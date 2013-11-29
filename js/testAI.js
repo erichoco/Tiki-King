7354f271c9e61657dab72b42484d12ced726c068
allyoushame <b99901025@ntu.edu.tw>
2013-11-29 20:35:11 +0800

    add comment in testAI.js

--- js/testAI.js
+++ js/testAI.js
@@ -1,17 +1,31 @@
 /* Implement AI here */
 
+//class state
+// remaining action , Tiki Order , 
+
+//var target
+
+
+//getAIAction(action) // for main.js to call
+//miniMax()
+//evaluation function()
+//updateState(action) // for main.js to call
+
 (function() {
 
 $(document).ready(function() {
 
         console.log("Test it");
         test();
+        console.log("change color");
+        tikiColor[0] = 'yellow';
 
 });
 
 function test() {
     console.log("test function");
 }
+
 
 
 })();
