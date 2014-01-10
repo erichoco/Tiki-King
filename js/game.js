0e2871bf78e2d33783dfe4e6220496c1164e5dde
allyoushame <b99901025@ntu.edu.tw>
2014-01-10 15:43:21 +0800

    update game.js

--- js/game.js
+++ js/game.js
@@ -1,7 +1,11 @@
 /*Each Round of Game implement in here */
 /*Each round of game exists a judge to handle the game*/
 
+
+//player1 -> player ; player2 -> AI
 var state = new State();
+var player1Score = NULL;
+var player2Score = NULL;
 
 // This is the 'class' of State
 function State() {
@@ -19,13 +23,77 @@ function State() {
 
 
 /*
- * reset the game , prepare for the new game
+ * Reset the game , prepare for the new game
  */
-function reset()
+function resetGame()
 {
-   Console.log("This is reset function!")
+   Console.log("This is reset function!")\
+   state = new State();
+   player1Score = NULL;
+   player2Score = NULL;
 
 }
+
+
+/*
+ * Tell game judge who are you , which Tiki you want to move , and what is your action
+ * Then Judge should update game state according to the request  
+ */
+
+
+ //problem: What should be the input ? tikiID or tikiIdx?
+function tellJudge(playerID , tikiIdx , action)
+{
+    Console.log("This is tellJudge function")
+
+    updateState(action , tikiIdx , playerID)
+    checkGameEnd()
+
+}
+
+
+/*
+ * Check whether the game is over
+ * If yes , assign the score
+ */
+function checkGameEnd()
+{
+    Console.log("This is checkGameEnd function")
+    if ((0 === state.humActions.length && 0 === state.comActions.length) || state.tikiOrder.length <= 3) 
+    {
+        computeResult( player1Score , player2Score);
+    }
+}
+
+/*
+ * called by 
+ * return scores
+ */
+function askJudge()
+{
+    Console.log("This is askJudge function")
+
+    if(player1Score == NULL || player2Score == NULL)
+        return NULL;
+}
+
+
+
+/***** Helper Functions *****/
+
+
+/*
+ * input is 2 scores
+ * one is Computer Score , one is Human
+ */
+function computeResult( winner  , comScore) {
+     comScore = computeAISocre(state), humScore = computeHumSocre(state);
+     winner = (comScore > humScore)? 'Computer' : 'You';
+    alert('Your score: ' + humScore +
+          '\nComputer\'s score: ' + comScore +
+          '\n' + winner + ' Wins');
+}
+
 
 
 function copyState( stateToBeCopied,targetState)
@@ -36,3 +104,102 @@ function copyState( stateToBeCopied,targetState)
     stateToBeCopied.comMission = targetState.comMission.slice(0);
     stateToBeCopied.humMission = targetState.humMission.slice(0);
 }
+
+// The following functions are related to update state
+function updateState(action, tikiIdx, player) {
+    //console.log('Hey, I receive action', action, 'on Tiki #' + tikiIdx);
+    var tikiId = state.tikiOrder[tikiIdx]
+
+    if (0 === player) {
+        var idx = state.comActions.indexOf(action);
+        state.comActions.splice(idx, 1);
+        console.log('This is AI do the action', action, tikiIdx)
+        if (action == 0) {
+            updateStateWithMoveup(1,tikiId , state);
+            moveUp(tikiIdx, 1);
+        }
+        else if (action == 1) {
+            updateStateWithMoveup(2,tikiId, state);
+            moveUp(tikiIdx, 2);
+        }
+        else if (action == 2) {
+            updateStateWithMoveup(3,tikiId, state);
+            moveUp(tikiIdx, 3);
+        }
+        else if (action == 3) {
+            updateStateWithPush(tikiId, state);
+            moveToBottom(tikiIdx);
+        }
+        else if (action == 4) {
+            updateStateWithKill(state);
+            killLast();
+        }
+    }
+    else if (1 === player) {
+
+        var idx = state.humActions.indexOf(action);
+        state.humActions.splice(idx, 1);
+        console.log('This is human do the action')
+
+        if (action == 0)
+            updateStateWithMoveup(1,tikiId , state);
+        else if (action == 1)
+            updateStateWithMoveup(2,tikiId, state);
+        else if (action == 2)
+            updateStateWithMoveup(3,tikiId, state); 
+        else if (action == 3)
+            updateStateWithPush(tikiId, state);
+        else if (action == 4)
+            updateStateWithKill(state);
+    }
+
+    else {
+        console.log('ERROR: invalid "player" passed to updateState()')
+    }
+
+    console.log('Current Order:', state.tikiOrder)
+    
+}
+
+
+
+
+function updateStateWithMoveup(moveup , tikiId , targetState)
+{
+
+    var tmp = tikiId
+    var moveTime = 0
+    var tikiIndex = getTikiIndex(tikiId , targetState)
+
+    while(moveTime<moveup){
+        var index = tikiIndex - moveTime
+        targetState.tikiOrder[index] = targetState.tikiOrder[index-1]
+        moveTime +=1
+    }
+    targetState.tikiOrder[tikiIndex - moveup] = tmp;
+}
+
+function updateStateWithPush(tikiId , targetState)
+{
+
+    var tikiIndex = getTikiIndex(tikiId , targetState);
+    var index = tikiIndex;
+
+
+    while(index<targetState.tikiOrder.length-1){
+        targetState.tikiOrder[index] = targetState.tikiOrder[index+1];
+        index +=1;
+    }
+
+    targetState.tikiOrder[targetState.tikiOrder.length-1] = tikiId;
+
+}
+
+function updateStateWithKill(targetState)
+{
+
+    targetState.tikiOrder.pop();
+
+}
+
+
